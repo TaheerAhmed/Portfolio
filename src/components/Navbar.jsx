dc88c02a893f4d7de9c827fb701ca6dda27ce5b8
TaheerAhmed <85991537+TaheerAhmed@users.noreply.github.com>
2023-07-15 16:42:36 +0530

    changed logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+          <img src={logo} alt='logo' className='w-9 h-9 object-contain fill-white rounded-xl' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Taheer Ahmed &nbsp;
             <span className='sm:block hidden'> | App Specialist</span>
